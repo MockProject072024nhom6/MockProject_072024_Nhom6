e2e87b192044876814ccc26278a1ba781041e3a7
phuc20057371 <foxfessor@gmail.com>
2024-06-08 21:19:13 +0700

    init project

---
+++ src/test/java/com/revolver/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revolver.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

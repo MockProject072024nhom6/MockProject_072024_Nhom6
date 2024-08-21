86e2562488a086a9ca20b3b91aa4fdc3fdbb9dee
Phuoc <khuathoduyphuoc2001@gmail.com>
2024-08-21 21:44:20 +0700

    Xịn quá ^^

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import { createRoot } from "react-dom/client"
 import App from "./App.tsx"
 import "./index.css"
 
-import "@fontsource/roboto"
-import "@fontsource/montserrat"
-import "@fontsource/palanquin"
+// import "@fontsource/roboto"
+// import "@fontsource/montserrat"
+// import "@fontsource/palanquin"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>

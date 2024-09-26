f1d8aaa21b52f0362fa96de1b8eda2652cc40d39
đạt nguyễn <ndaten0@gmail.com>
2024-09-26 07:47:06 +0700

    edit EditableButtonInput

--- src/Components/PersonalInformationComponents/EditableButtonInput.tsx
+++ src/Components/PersonalInformationComponents/EditableButtonInput.tsx
@@ -1,85 +1,77 @@
-import React, { useState } from 'react';
-import { I_props_EditableButton } from '../../Types/GuardTypes';
+import React, { useState } from "react"
+import { I_props_EditableButton } from "../../Types/GuardTypes"
 
-const EditableButtonInput: React.FC<I_props_EditableButton> = ({
-  initialText,
-  type,
-  label,
-}) => {
-  const [isEditing, setIsEditing] = useState(false);
-  const [text, setText] = useState(initialText);
-  const [showPassword, setShowPassword] = useState(
-    type === 'password' ? false : true
-  ); // Điều kiện khởi tạo
+const EditableButtonInput: React.FC<I_props_EditableButton> = ({ initialText, type, label }) => {
+  const [isEditing, setIsEditing] = useState(false)
+  const [text, setText] = useState(initialText)
+  const [showPassword, setShowPassword] = useState(type === "password" ? false : true)
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setText(e.target.value);
-  };
+    setText(e.target.value)
+  }
 
   const handleBlur = () => {
-    setIsEditing(false);
-  };
+    setIsEditing(false)
+  }
 
   const handleEditClick = () => {
-    setIsEditing(true);
-  };
+    setIsEditing(true)
+  }
 
   const toggleShowPassword = () => {
-    setShowPassword(prev => !prev);
-  };
+    setShowPassword(prev => !prev)
+  }
   return (
     <section>
-      <div className="relative p-2 border-2 my-1 rounded-lg flex items-center w-9/12">
-        <div className="items-center ml-2 truncate">{label}</div>
-        <div className="flex  items-center flex-grow justify-end ">
-          <div className="relative flex justify-end mr-2  flex-grow ">
+      <div className='relative p-2 border-2 my-1 rounded-lg flex items-center w-9/12'>
+        <div className='items-center ml-2 truncate'>{label}</div>
+        <div className='flex  items-center flex-grow justify-end '>
+          <div className='relative flex justify-end mr-2  flex-grow '>
             {isEditing ? (
               <input
-                type={showPassword ? 'text' : 'password'}
+                type={showPassword ? "text" : "password"}
                 value={text}
                 onChange={handleChange}
                 onBlur={handleBlur}
+                title={label}
+                placeholder={label}
                 autoFocus
-                className="px-3 py-1 rounded-lg max-w-72 w-full h-8 bg-gray-200 border border-gray-300 focus:outline-none text-start"
+                className='px-3 py-1 rounded-lg max-w-72 w-full h-8 bg-gray-200 border border-gray-300 focus:outline-none text-start'
               />
-            ) : type !== 'password' ? (
+            ) : type !== "password" ? (
               <button
                 onDoubleClick={handleEditClick}
-                className="px-3 py-1 rounded-lg text-gray-400 max-w-72 w-full h-8 bg-gray-200 hover:bg-blue-600 text-start truncate"
+                className='px-3 py-1 rounded-lg text-gray-400 max-w-72 w-full h-8 bg-gray-200 hover:bg-blue-600 text-start truncate'
               >
                 {text}
               </button>
             ) : (
               <input
-                type={showPassword ? 'text' : 'password'}
+                type={showPassword ? "text" : "password"}
                 value={text}
                 onChange={handleChange}
                 onBlur={handleBlur}
+                title={label}
+                placeholder={label}
                 autoFocus
-                className="pl-3 py-1 rounded-lg h-8 bg-gray-200 border border-gray-300 focus:outline-none text-start"
+                className='pl-3 py-1 rounded-lg h-8 bg-gray-200 border border-gray-300 focus:outline-none text-start'
               />
             )}
           </div>
 
-          {type === 'password' && (
-            <button
-              onClick={toggleShowPassword}
-              className="px-3 py-1 mr-2 rounded-lg bg-gray-300 hover:bg-gray-400"
-            >
-              {showPassword ? 'Ẩn' : 'Hiện'}
+          {type === "password" && (
+            <button onClick={toggleShowPassword} className='px-3 py-1 mr-2 rounded-lg bg-gray-300 hover:bg-gray-400'>
+              {showPassword ? "Hide" : "Show"}
             </button>
           )}
 
-          <button
-            onClick={handleEditClick}
-            className="px-3 mr-2 py-2  rounded-lg bg-gray-300 hover:bg-gray-400"
-          >
-            Sửa
+          <button onClick={handleEditClick} className='px-3 mr-2 py-2  rounded-lg bg-gray-300 hover:bg-gray-400'>
+            Edit
           </button>
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default EditableButtonInput;
+export default EditableButtonInput
